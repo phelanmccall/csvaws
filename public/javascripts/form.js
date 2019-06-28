01e3d08169210750210519dd28e2bff470e65bb4
Phelan <phelanmccall@yahoo.com>
2019-06-28 03:11:06 -0700

    everything seems to be working

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,54 +1,54 @@
-(function(){
+(function () {
     function csvSubmit(event) {
-        console.log(event.target);
+
         event.preventDefault();
         let formData = new FormData();
-        formData.append("data", event.target[1].files[0])
-        let csvName = event.target[1].value;
-        let fileName = csvName.substring(csvName.lastIndexOf("\\")+1, csvName.lastIndexOf('.'))
-        console.log(fileName)
+        formData.append("data", event.target[0].files[0])
+        let csvName = event.target[0].value;
+        let fileName = csvName.substring(csvName.lastIndexOf("\\") + 1, csvName.lastIndexOf('.'))
         let actionURL = `/csv/${fileName}`;
         axios.post(actionURL, formData, {
             headers: {
-              'Content-Type': 'multipart/form-data'
+                'Content-Type': 'multipart/form-data'
             }
         })
             .then(function (res) {
-                console.log(res.data);
+                document.getElementById("postConfirm").innerHTML = "Success";
+
+
             })
-            .catch(function(err){
+            .catch(function (err) {
                 console.log(err);
+                document.getElementById("postConfirm").innerHTML = err.message;
+
             });
-    
+
     }
 
-    function csvGet(event){
+    function csvGet(event) {
         event.preventDefault();
-        console.log(event.target);
-        axios.get("/csv/"+event.target.name.value).then((res)=>{
-            console.log(res.data);
+        axios.get("/csv/" + event.target.name.value).then((res) => {
             document.getElementById("resultDiv").innerHTML = res.data
         })
-        .catch((err)=>{
-            console.log(err.message);
-            document.getElementById("resultDiv").innerHTML = err.message
-        });  
+            .catch((err) => {
+                console.log(err);
+                document.getElementById("resultDiv").innerHTML = err.message
+            });
     }
-    function jsonGet(event){
+    function jsonGet(event) {
         event.preventDefault();
-        axios.get("/json/"+event.target.name.value).then((res)=>{
-            console.log(res.data);
+        axios.get("/json/" + event.target.name.value).then((res) => {
             document.getElementById("resultDiv").innerHTML = JSON.stringify(res.data);
         })
-        .catch((err)=>{
-            console.log(err.message);
-            document.getElementById("resultDiv").innerHTML = err.message
-        });  
+            .catch((err) => {
+                console.log(err);
+                document.getElementById("resultDiv").innerHTML = err.message
+            });
     }
     let csvform = document.getElementById("csvform");
-    csvform.addEventListener("submit",csvSubmit);
+    csvform.addEventListener("submit", csvSubmit);
     let csvget = document.getElementById("csvget");
-    csvget.addEventListener("submit",csvGet);
+    csvget.addEventListener("submit", csvGet);
     let jsonget = document.getElementById("jsonget");
-    jsonget.addEventListener("submit",jsonGet);
- }());
+    jsonget.addEventListener("submit", jsonGet);
+}());
